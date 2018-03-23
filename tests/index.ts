90259b8fd2a9f9e135eb65c0bc29a7dc644474c3
Kris Selden <kris.selden@gmail.com>
2018-03-23 15:14:05 -0700

    bump rest of deps

--- tests/index.ts
+++ tests/index.ts
@@ -1,4 +1,9 @@
-import { createBuilder, createTempDir, Disposable, TempDir } from 'broccoli-test-helper';
+import {
+  createBuilder,
+  createTempDir,
+  Disposable,
+  TempDir,
+} from 'broccoli-test-helper';
 import Rollup = require('../index');
 // tslint:disable-next-line:no-var-requires
 const mergeTrees: (input: any[]) => any = require('broccoli-merge-trees');
@@ -8,8 +13,8 @@ const it = QUnit.test;
 // tslint:disable:max-line-length
 
 describe('Staging files smoke tests', () => {
-  it('handles merged trees and building from staging', async (assert) => {
-    await using(async (use) => {
+  it('handles merged trees and building from staging', async assert => {
+    await using(async use => {
       const input1 = use(await createTempDir());
       const input2 = use(await createTempDir());
       const node = new Rollup(mergeTrees([input1.path(), input2.path()]), {
@@ -25,26 +30,29 @@ describe('Staging files smoke tests', () => {
 
       input1.write({
         'add.js': 'export const add = num => num++;',
-        'index.js': 'import two from "./two"; import { add } from "./add"; const result = add(two); export default result;',
+        'index.js':
+          'import two from "./two"; import { add } from "./add"; const result = add(two); export default result;',
       });
 
       input2.write({
         'minus.js': 'export const minus = num => num--;',
-        'two.js': 'import { minus } from "./minus"; const two = minus(3); export default two;',
+        'two.js':
+          'import { minus } from "./minus"; const two = minus(3); export default two;',
       });
 
       await output.build();
 
       assert.deepEqual(output.read(), {
-        'out.js': 'const minus = num => num--;\n\nconst two = minus(3);\n\nconst add = num => num++;\n\nconst result = add(two);\n\nexport default result;\n',
+        'out.js':
+          'const minus = num => num--;\n\nconst two = minus(3);\n\nconst add = num => num++;\n\nconst result = add(two);\n\nexport default result;\n',
       });
     });
   });
 });
 
 describe('BroccoliRollup', () => {
-  it('test build: initial update noop', async (assert) => {
-    await using(async (use) => {
+  it('test build: initial update noop', async assert => {
+    await using(async use => {
       const input = use(await createTempDir());
       const subject = new Rollup(input.path(), {
         rollup: {
@@ -56,14 +64,15 @@ describe('BroccoliRollup', () => {
         },
       });
       const output = use(createBuilder(subject));
-        // INITIAL
+      // INITIAL
       input.write({
         'add.js': 'export default x => x + x;',
-        'index.js': 'import add from "./add"; const two = add(1); export default two;',
+        'index.js':
+          'import add from "./add"; const two = add(1); export default two;',
       });
       await output.build();
 
-      assert.deepEqual(output.read() , {
+      assert.deepEqual(output.read(), {
         'out.js': `var add = x => x + x;
 
 const two = add(1);
@@ -89,11 +98,11 @@ const two = add(1);
 export default two;
 `,
       });
-      assert.deepEqual(output.changes(), {
-      });
+      assert.deepEqual(output.changes(), {});
 
       input.write({
-        'index.js': 'import add from "./add"; import minus from "./minus"; export default { a: add(1), b: minus(1) };',
+        'index.js':
+          'import add from "./add"; import minus from "./minus"; export default { a: add(1), b: minus(1) };',
       });
 
       await output.build();
@@ -112,7 +121,7 @@ export default index;
         'out.js': 'change',
       });
 
-      input.write({ 'minus.js':  null });
+      input.write({ 'minus.js': null });
 
       let errorWasThrown = false;
       try {
@@ -148,13 +157,15 @@ export default index;
     });
   });
 
-  describe('targets', (hooks) => {
+  describe('targets', hooks => {
     let input: TempDir;
     hooks.beforeEach(async () => {
       input = await createTempDir();
       input.write({
         'add.js': 'export default x => x + x;',
-        'index.js': 'import add from "./add"; const two = add(1); export default two;'});
+        'index.js':
+          'import add from "./add"; const two = add(1); export default two;',
+      });
     });
 
     hooks.afterEach(async () => {
@@ -162,15 +173,17 @@ export default index;
     });
 
     // supports multiple targets
-    it('works with one explicit target', async (assert) => {
+    it('works with one explicit target', async assert => {
       const node = new Rollup(input.path(), {
         rollup: {
           input: 'index.js',
-          output: [{
+          output: [
+            {
               file: 'dist/out.umd.js',
               format: 'umd',
               name: 'thing',
-          }],
+            },
+          ],
         },
       });
       const output = createBuilder(node);
@@ -185,62 +198,70 @@ export default index;
 	(global.thing = factory());
 }(this, (function () { 'use strict';
 
-var add = x => x + x;
+	var add = x => x + x;
 
-const two = add(1);
+	const two = add(1);
 
-return two;
+	return two;
 
-})));\n`}});
+})));
+`,
+          },
+        });
       } finally {
         await output.dispose();
       }
     });
 
-    it('works with many targets', async (assert) => {
+    it('works with many targets', async assert => {
       const node = new Rollup(input.path(), {
         rollup: {
           input: 'index.js',
-          output: [{
-            file: 'dist/out.umd.js',
-            format: 'umd',
-            name: 'thing',
-          }, {
-            file: 'dist/out.js',
-            format: 'es',
-            sourcemap: true,
-          }],
+          output: [
+            {
+              file: 'dist/out.umd.js',
+              format: 'umd',
+              name: 'thing',
+            },
+            {
+              file: 'dist/out.js',
+              format: 'es',
+              sourcemap: true,
+            },
+          ],
         },
       });
 
       const output = createBuilder(node);
       try {
-
         await output.build();
 
         assert.deepEqual(output.read(), {
           dist: {
-          'out.js': `var add = x => x + x;
+            'out.js': `var add = x => x + x;
 
 const two = add(1);
 
 export default two;
 //# sourceMappingURL=out.js.map`,
-          'out.js.map': '{"version":3,"file":"out.js","sources":["../add.js","../index.js"],"sourcesContent":["export default x => x + x;","import add from \\"./add\\"; const two = add(1); export default two;"],"names":[],"mappings":"AAAA,UAAe,CAAC,IAAI,CAAC,GAAG,CAAC;;qBAAC,rBCAD,MAAM,GAAG,GAAG,GAAG,CAAC,CAAC,CAAC;;;;"}',
-          'out.umd.js': `(function (global, factory) {
+            'out.js.map':
+              '{"version":3,"file":"out.js","sources":["../add.js","../index.js"],"sourcesContent":["export default x => x + x;","import add from \\"./add\\"; const two = add(1); export default two;"],"names":[],"mappings":"AAAA,UAAe,CAAC,IAAI,CAAC,GAAG,CAAC;;qBAAC,rBCAD,MAAM,GAAG,GAAG,GAAG,CAAC,CAAC,CAAC,CAAC;;;;"}',
+            'out.umd.js': `(function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
 	typeof define === 'function' && define.amd ? define(factory) :
 	(global.thing = factory());
 }(this, (function () { 'use strict';
 
-var add = x => x + x;
+	var add = x => x + x;
 
-const two = add(1);
+	const two = add(1);
 
-return two;
+	return two;
 
-})));\n`,
-          }});
+})));
+`,
+          },
+        });
       } finally {
         await output.dispose();
       }
@@ -248,23 +269,29 @@ return two;
   });
 
   describe('passing nodeModulesPath', () => {
-    it('should throw if nodeModulesPath is relative', (assert) => {
-      assert.throws(() => new Rollup('lib', {
-        nodeModulesPath: './',
-        rollup: {
-          input: 'index.js',
-          output: {
-            file: 'out.js',
-            format: 'es',
-          },
-        },
-      }), new Error('nodeModulesPath must be fully qualified and you passed a relative path'));
+    it('should throw if nodeModulesPath is relative', assert => {
+      assert.throws(
+        () =>
+          new Rollup('lib', {
+            nodeModulesPath: './',
+            rollup: {
+              input: 'index.js',
+              output: {
+                file: 'out.js',
+                format: 'es',
+              },
+            },
+          }),
+        new Error(
+          'nodeModulesPath must be fully qualified and you passed a relative path',
+        ),
+      );
     });
   });
 
   describe('tree shaking', () => {
-    it('can code split', async (assert) => {
-      await using(async (use) => {
+    it('can code split', async assert => {
+      await using(async use => {
         const input = use(await createTempDir());
         const subject = new Rollup(input.path(), {
           rollup: {
@@ -280,8 +307,10 @@ return two;
         const output = use(createBuilder(subject));
 
         input.write({
-          'a.js': 'import c from "./c"; import e from "./e"; export const out = c + e;',
-          'b.js': 'import d from "./d";import e from "./e"; export const out = d + e;',
+          'a.js':
+            'import c from "./c"; import e from "./e"; export const out = c + e;',
+          'b.js':
+            'import d from "./d";import e from "./e"; export const out = d + e;',
           'c.js': 'const num1 = 1; export default num1;',
           'd.js': 'const num2 = 2; export default num2;',
           'e.js': 'const num3 = 3; export default num3;',
@@ -291,25 +320,25 @@ return two;
 
         assert.deepEqual(output.read(), {
           chunks: {
-            'a.js': `import { e as num3 } from './chunk1.js';
+            'a.js': `import e from './chunk1.js';
 
 const num1 = 1;
 
-const out = num1 + num3;
+const out = num1 + e;
 
 export { out };
 `,
-            'b.js': `import { e as num3 } from './chunk1.js';
+            'b.js': `import e from './chunk1.js';
 
 const num2 = 2;
 
-const out = num2 + num3;
+const out = num2 + e;
 
 export { out };
 `,
             'chunk1.js': `const num3 = 3;
 
-export { num3 as e };
+export default num3;
 `,
           },
         });
@@ -323,7 +352,7 @@ type UseCallback = <T extends Disposable>(disposable: T) => T;
 // tslint:disable:no-conditional-assignment
 async function using(body: (use: UseCallback) => Promise<void>) {
   const disposables: Disposable[] = [];
-  const use: UseCallback = (disposable) => {
+  const use: UseCallback = disposable => {
     disposables.push(disposable);
     return disposable;
   };
@@ -331,7 +360,7 @@ async function using(body: (use: UseCallback) => Promise<void>) {
     await body(use);
   } finally {
     let disposable;
-    while (disposable = disposables.pop()) {
+    while ((disposable = disposables.pop())) {
       await disposable.dispose();
     }
   }
